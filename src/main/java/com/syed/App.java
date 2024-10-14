91c761e8ef6f6abc5842f05cecd98cf596dea492
syedazmathulla <syedazmathulla4@gmail.com>
2024-10-14 13:02:34 +0530

    This is first commit

---
+++ src/main/java/com/syed/App.java
@@ -0,0 +1,10 @@
+package com.syed;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World! This is Syed Azmathulla");
+    }
+}
